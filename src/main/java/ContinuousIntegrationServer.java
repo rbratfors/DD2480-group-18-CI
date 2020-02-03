a58469424bb4783d5df5d728d874bf2f3f2a9dc6
Johan moritz <jmoritz@kth.se>
2020-02-03 16:27:59 +0100

    and

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -55,8 +55,6 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         // for example
         // 1st clone your repository
 
-        //try again
-
         try {
             Git git = Git.cloneRepository().setURI(cloneUrl).setDirectory(new File("./cloned")).call();
             Repository repository = git.getRepository();
