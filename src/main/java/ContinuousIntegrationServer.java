068add62826b821d46d1e57898e2e3fed5b86eb3
Johan moritz <jmoritz@kth.se>
2020-02-03 16:07:41 +0100

    Again

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -12,23 +12,23 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand.ListMode;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import org.json.JSONObject;
 
-
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class ContinuousIntegrationServer extends AbstractHandler
-{
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
-        throws IOException, ServletException
-    {
+/**
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook See the Jetty
+ * documentation for API documentation of those classes.
+ */
+public class ContinuousIntegrationServer extends AbstractHandler {
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -37,7 +37,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         JSONObject json = new JSONObject(request.getParameter("payload"));
         // for (String key : json.keySet()) {
-        //     System.out.println(key);
+        // System.out.println(key);
         // }
 
         JSONObject repo = json.getJSONObject("repository");
@@ -51,13 +51,10 @@ public class ContinuousIntegrationServer extends AbstractHandler
         String branchRef = json.getString("ref");
         System.out.println(branchRef);
 
-
-
-
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
-        
+
         try {
             Git git = Git.cloneRepository().setURI(cloneUrl).call();
             Repository repository = git.getRepository();
@@ -74,6 +71,19 @@ public class ContinuousIntegrationServer extends AbstractHandler
             for (Ref branch : branches) {
                 System.out.println(branch.getName());
             }
+
+            ObjectId head  = repository.resolve(Constants.HEAD);
+
+            try (RevWalk revWalk = new RevWalk(repository)) {
+                RevCommit commit = revWalk.parseCommit(head);
+    
+                System.out.println("Time of commit (seconds since epoch): " + commit.getCommitTime());
+    
+                // and using commit's tree find the path
+                RevTree tree = commit.getTree();
+                System.out.println("Having tree: " + tree);
+            }
+            
         } catch (GitAPIException e) {
             e.printStackTrace();
         }
