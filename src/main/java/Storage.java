ee57449b9d7cecbe4a7413665495cde0b8b6acf7
Gabriel Gessle <sandshrew97@gmail.com>
2020-02-03 17:31:21 +0100

    Add post function
    Post function now works as intended and adds the storage object (with a maybe to change format) to the database.
    
    Related to issue #17 and #25

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -7,8 +7,14 @@ import java.io.File;
  
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Storage {
+    String fileName = "db.json";
     /**
      * Creates a new json data object if none exists already
      */
@@ -19,7 +25,7 @@ public class Storage {
             JSONObject data = new JSONObject();
 
             // Write to JSON file
-            try (FileWriter file = new FileWriter("db.json")) {
+            try (FileWriter file = new FileWriter(fileName)) {
                 file.write(data.toString());
                 file.flush();
             } catch (IOException e) {
@@ -29,10 +35,44 @@ public class Storage {
     }
 
     /**
-     * Adds a new commit to the data object
+     * Taken from https://stackoverflow.com/questions/7463414/what-s-the-best-way-to-load-a-jsonobject-from-a-json-text-file
+     * @param file name of file name
+     * @return json object of the file given
+     * @throws JSONException
+     * @throws IOException
      */
-    public void post() {
-        
+    private JSONObject parseJSON(String file) throws JSONException, IOException {
+        String content = new String(Files.readAllBytes(Paths.get(file)));
+        return new JSONObject(content);
+    }
+
+    /**
+     * Adds a new commit to the data object
+     * @param id id of the commit
+     * @param passed checks passed
+     * @param header commit header
+     * @param body commit message
+     */
+    public void post(String id, boolean passed, String header, String body) throws IOException {
+        File db = new File(fileName);
+        if (!db.exists()) {
+            throw new IOException("Database file not found");
+        } else {
+            JSONObject dbJSON = parseJSON(fileName);
+            JSONObject data = new JSONObject();
+            data.put("Passed", passed);
+            data.put("Header", header);
+            data.put("Body", body);
+            dbJSON.put(id, data);
+
+            // Write to JSON file
+            try (FileWriter file = new FileWriter(fileName)) {
+                file.write(dbJSON.toString());
+                file.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
