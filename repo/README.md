3dfb28d5e3138a04214c44637f4b5718b889b3b6
rbratfors <rbratfors@gmail.com>
2020-02-04 15:58:42 +0100

    Refactor - removed test repo directory

--- repo/README.md
+++
@@ -1,2 +0,0 @@
-# DD2480-group-18-CI
-A simple continuos integration solution, created for assignment 2 at KTH course DD2480. Spring of 2020.
--- repo/src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	public static void main(String args[]) {
-		System.out.println("Test will commence printing now");
-		for(String arg : args) {
-			System.out.println(arg);
-		}
-	}
-}
