9b51d009e003d4321303da62eb88b60db2a83234
Gabriel Gessle <sandshrew97@gmail.com>
2020-02-05 17:03:05 +0100

    Implement local storage (#26)
    * Add Storage class
    
    Add initial format for Storage class, with a constructor implementation that creates a new local json file if none exists already.
    
    Two functions will be available for use;
    * Get
    * Post
    
    Which will work similar to most APIs where we are able to get and post into the json storage object.
    
    Related to issue #17
    
    * Add post function
    
    Post function now works as intended and adds the storage object (with a maybe to change format) to the database.
    
    Related to issue #17 and #25
    
    * Add Get function
    
    Get function should now work as intended and fetches a given object from the database depending on its id. If key was not found in object, then return an empty object.
    
    Related to issue #17 and #25
    
    * Add gradle dependencies
    
    Add some new gradle dependencies that were used for Storage
    
    * Integrate Build class into storage
    
    Storage and API skeleton code are now working together. API is now able to return a list of Build objects that should be fetched from the frontend. This is done by performing a get request to <your local address>/ci/get and you will get a reply in the following format:
    
    * jobID
      * status
      * commitSha
      * url
      * log
    
    Add getAll function in Storage class so that all builds can be fetched at the same time for performance and ease of use reasons.
    
    Related to issue #26
    
    * Fix naming
    
    Naming was broken after merge, should be fixed now.
    
    * Refactor Resource.java
    
    Move the chunk of code that converts a json object to a Build object into a seperate function

--- src/main/java/resources/Resource.java
+++ src/main/java/resources/Resource.java
@@ -5,6 +5,8 @@ import buildtools.BuildJob;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,17 +16,72 @@ import javax.ws.rs.core.Response;
 
 @Path("ci")
 public class Resource {
+    Storage storage = new Storage();
+
+    /**
+     * Creates a Build object from a given json object and its key (jobID)
+     * @param key jobID
+     * @param o rest of keys
+     * @return Build object created from given json
+     */
+    private Build json2Build(String key, JSONObject o) throws IOException {
+        String jobID = key;
+        String commitSha = o.getString("commitSha");
+        String url = o.getString("url");
+        String log = o.getString("log");
+
+        // fix status
+        String tempStatus = o.getString("status");
+        Build.Result status;
+        switch (tempStatus) {
+            case "success":
+                status = Build.Result.success;
+                break;
+            case "pending":
+                status = Build.Result.pending;
+                break;
+            case "failure":
+                status = Build.Result.failure;
+                break;
+            case "error":
+                status = Build.Result.error;
+                break;
+            default:
+                throw new IOException("Invalid 'status' in database");
+        }
+
+        Build b = new Build(jobID, status, commitSha, url, log);
+
+        return b;
+    }
+
+    /**
+     * Fetches all builds from local database
+     * path: /ci/get
+     * @return
+     * @throws IOException
+     */
 
     ExecutorService jobsQueue = Executors.newFixedThreadPool(10);
-    // EXAMPLE GET REQUEST HANDLER
+
     // A list of of builds is sent by returning List<Build> instead.
     // TODO
     // load a previous build from storage
     // load multiple previous builds from storage
     @GET
+    @Path("get")
     @Produces("application/json")
-    public Build hello() {
-        return new Build("jobID", "status", "commitSha", "url", "log");
+    public ArrayList<Build> getBuilds() throws IOException {
+        JSONObject dbJSON = storage.getAll();
+        ArrayList<Build> l = new ArrayList<Build>();
+
+        // go through every job
+        for (String key : dbJSON.keySet()) {
+            Build b = json2Build(key, dbJSON.getJSONObject(key));
+            l.add(b);
+        }
+
+        return l;
     }
 
     // EXAMPLE POST REQUEST HANDLER
