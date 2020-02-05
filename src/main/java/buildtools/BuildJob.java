2d7df3d4549872ab6e26538a2bce895bd53c2fd9
Johan moritz <jmoritz@kth.se>
2020-02-05 17:35:29 +0100

    Reflect build status in database
    Resolves issue #39.

--- src/main/java/buildtools/BuildJob.java
+++ src/main/java/buildtools/BuildJob.java
@@ -1,18 +1,33 @@
 package buildtools;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 
+import resources.Storage;
 
 public class BuildJob {
   public static String BUILD_CONFIG_FILE_NAME = ".dd.yml";
+  private static Storage storage = new Storage();
   public static void run(String jobID, String cloneURL, String branchRef) {
     // TODO: update database that status is "started"
     System.out.println("Running build job with id " + jobID);
+
+    Build pendingBuild = new Build(jobID, Build.Result.pending, "", "", "");
+
+    try {
+      BuildJob.storage.post(pendingBuild);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      BuildJob.fail(jobID, "Internal issue. Contact support.");
+      return;
+    }
+
 
     Git git = null;
     try {
@@ -25,6 +40,7 @@ public class BuildJob {
     } catch (GitAPIException e) {
       e.printStackTrace();
       BuildJob.fail(jobID, "Failed to clone repository " + cloneURL);
+      return;
     }
 
     Repository repository = git.getRepository();
@@ -56,8 +72,16 @@ public class BuildJob {
 
   public static void fail(String jobID, String log) {
     // TODO:
-    // Update database with status and log
     // Call notification engine
+
+    Build failedBuild = new Build(jobID, Build.Result.failure, "", "", "");
+    try {
+      BuildJob.storage.post(failedBuild);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
     
     // Mock:
     System.out.println("Failed job " + jobID + " with log ");
@@ -67,8 +91,16 @@ public class BuildJob {
 
   public static void success(String jobID, String log) {
     // TODO:
-    // Update database with status and log
     // Call notification engine
+
+    Build succeededBuild = new Build(jobID, Build.Result.success, "", "", "");
+    try {
+      BuildJob.storage.post(succeededBuild);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
 
     // Mock:
     System.out.println("Succeeded job " + jobID + " with log ");
