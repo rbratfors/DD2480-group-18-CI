93731556ef53e0634ea982102addca2338d3dd1b
Johan moritz <jmoritz@kth.se>
2020-02-05 16:38:14 +0100

    Run build jobs asyncronously
    Se issue #37 .
    
    Github shouldn't have to wait for build jobs to finish to get a response on a webhook. We solve this by using an Executor service.

--- src/main/java/resources/Resource.java
+++ src/main/java/resources/Resource.java
@@ -6,12 +6,16 @@ import buildtools.BuildJob;
 import org.json.JSONObject;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("ci")
 public class Resource {
+
+    ExecutorService jobsQueue = Executors.newFixedThreadPool(10);
     // EXAMPLE GET REQUEST HANDLER
     // A list of of builds is sent by returning List<Build> instead.
     // TODO
@@ -51,7 +55,9 @@ public class Resource {
         String branchRef = json.getString("ref");
         String cloneUrl = repository.getString("clone_url");
 
-        BuildJob.run(jobID, cloneUrl, branchRef);
+        // Run build jobs asynchronously
+        Runnable job = () -> BuildJob.run(jobID, cloneUrl, branchRef);
+        jobsQueue.execute(job);
 
         return Response.status(200).build();
     }
