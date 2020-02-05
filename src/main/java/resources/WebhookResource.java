48b728a094f7788eb0ee18d518c59042fb60cd5a
Gabriel Gessle <sandshrew97@gmail.com>
2020-02-05 15:18:22 +0100

    Integrate Build class into storage
    Storage and API skeleton code are now working together. API is now able to return a list of Build objects that should be fetched from the frontend. This is done by performing a get request to <your local address>/ci/get and you will get a reply in the following format:
    
    * jobID
      * status
      * commitSha
      * url
      * log
    
    Add getAll function in Storage class so that all builds can be fetched at the same time for performance and ease of use reasons.
    
    Related to issue #26

--- src/main/java/resources/WebhookResource.java
+++ src/main/java/resources/WebhookResource.java
@@ -2,33 +2,38 @@ package resources;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.UUID;
+import java.util.ArrayList;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("ci")
 public class WebhookResource {
+    Storage storage = new Storage();
+    public enum Result {
+        pending,
+        success,
+        failure,
+        error
+    }
+
     // Create "build" class for storing and sending
     public class Build {
-        private String message;
+        public String log;
+        public String jobID;
+        public String commitSha;
+        public String url;
+        public Result status;
 
-        public Build(String name) {
-            this.message = getGreeting(name);
+        public Build(String log, String jobID, String commitSha, String url, Result status) {
+            this.log = log;
+            this.jobID = jobID;
+            this.commitSha = commitSha;
+            this.url = url;
+            this.status = status;
         }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String name) {
-            this.message = name;
-        }
-
-        private String getGreeting(String name) {
-            return "Hello " + name;
-        }
-
     }
 
     // EXAMPLE GET REQUEST HANDLER
@@ -37,9 +42,45 @@ public class WebhookResource {
     // load a previous build from storage
     // load multiple previous builds from storage
     @GET
+    @Path("get")
     @Produces("application/json")
-    public Build hello() {
-        return new Build("genius!");
+    public ArrayList<Build> getBuilds() throws IOException {
+        JSONObject dbJSON = storage.getAll();
+        ArrayList<WebhookResource.Build> l = new ArrayList<WebhookResource.Build>();
+
+        // go through every job
+        for (String key : dbJSON.keySet()) {
+            JSONObject current = dbJSON.getJSONObject(key);
+            String jobID = key;
+            String commitSha = current.getString("commitSha");
+            String url = current.getString("url");
+            String log = current.getString("log");
+
+            // fix status
+            String tempStatus = current.getString("status");
+            Result status;
+            switch (tempStatus) {
+                case "success":
+                    status = Result.success;
+                    break;
+                case "pending":
+                    status = Result.pending;
+                    break;
+                case "failure":
+                    status = Result.failure;
+                    break;
+                case "error":
+                    status = Result.error;
+                    break;
+                default:
+                    throw new IOException("Invalid 'status' in database");
+            }
+
+            Build b = new Build(log, jobID, commitSha, url, status);
+            l.add(b);
+        }
+
+        return l;
     }
 
     // EXAMPLE POST REQUEST HANDLER
