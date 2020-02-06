7fe1c0308443093999b182ca57fe4190a9a7d036
Johan moritz <jmoritz@kth.se>
2020-02-06 16:06:35 +0100

    Migrate log schema to arrays
    Resolves issue #42.
    
    Instead of a String build log we represent it as an array of arrays. Each outer array represent a group of log lines. Each inner array represent the lines.

--- src/main/java/buildtools/BuildJob.java
+++ src/main/java/buildtools/BuildJob.java
@@ -3,6 +3,7 @@ package buildtools;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -14,17 +15,30 @@ public class BuildJob {
   public static String BUILD_CONFIG_FILE_NAME = ".dd.yml";
   private static Storage storage = new Storage();
   public static void run(String jobID, String cloneURL, String branchRef) {
+    List<ArrayList<String>> log = new ArrayList<ArrayList<String>>();
+    ArrayList<String> logEntry = new ArrayList<String>();
+    logEntry.add("Running build job with id " + jobID);
+    log.add(logEntry);
+
+
     System.out.println("Running build job with id " + jobID);
-    Build pendingBuild = new Build(jobID, Build.Result.pending, "", "", "");
+    Build pendingBuild = new Build(jobID, Build.Result.pending, "", "", log);
 
     try {
       BuildJob.storage.post(pendingBuild);
 
     } catch (IOException e) {
       e.printStackTrace();
-      BuildJob.fail(jobID, "Internal issue. Contact support.");
+      logEntry.clear();
+      logEntry.add("Internal issue. Contact support.");
+      log.add(logEntry);
+      BuildJob.fail(jobID, log);
       return;
     }
+
+    logEntry.clear();
+    logEntry.add("Cloning repository.");
+    log.add(logEntry);
 
 
     Git git = null;
@@ -37,7 +51,10 @@ public class BuildJob {
 
     } catch (GitAPIException e) {
       e.printStackTrace();
-      BuildJob.fail(jobID, "Failed to clone repository " + cloneURL);
+      logEntry.clear();
+      logEntry.add("Failed to clone repository " + cloneURL);
+      log.add(logEntry);
+      BuildJob.fail(jobID, log);
       return;
     }
 
@@ -60,19 +77,29 @@ public class BuildJob {
           System.out.println(ln);
         }
       }
-      BuildJob.success(jobID, "Found build file.");
+
+      logEntry.clear();
+      logEntry.add("Found build file.");
+      log.add(logEntry);
+
+      BuildJob.success(jobID, log);
     } else {
-      BuildJob.fail(jobID, "Failed to find a build file.");
+
+      logEntry.clear();
+      logEntry.add("Failed to find a build file.");
+      log.add(logEntry);
+
+      BuildJob.fail(jobID, log);
     }
 
     System.out.println("Finished build job with id " + jobID);
   }
 
-  public static void fail(String jobID, String log) {
+  public static void fail(String jobID, List<ArrayList<String>> log) {
     // TODO:
     // Call notification engine
 
-    Build failedBuild = new Build(jobID, Build.Result.failure, "", "", "");
+    Build failedBuild = new Build(jobID, Build.Result.failure, "", "", log);
     try {
       BuildJob.storage.post(failedBuild);
 
@@ -86,11 +113,11 @@ public class BuildJob {
 
   }
 
-  public static void success(String jobID, String log) {
+  public static void success(String jobID, List<ArrayList<String>> log) {
     // TODO:
     // Call notification engine
 
-    Build succeededBuild = new Build(jobID, Build.Result.success, "", "", "");
+    Build succeededBuild = new Build(jobID, Build.Result.success, "", "", log);
     try {
       BuildJob.storage.post(succeededBuild);
 
