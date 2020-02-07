9e98d79ab2c5135411a7a5b6ebe9c1efa28a7b54
Johan moritz <jmoritz@kth.se>
2020-02-07 11:06:45 +0100

    Add junit boilerplate

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,11 @@
+import org.junit.Test;
 
-class ExampleTest {
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleTest {
   @Test
-  void FailingTest() {
+  public void FailingTest() {
     assertTrue(false);
   }
 }
